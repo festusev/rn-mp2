8f8972b45b5dab2367ef527f952e3e84775a08c4
festusev <evandellis@gmail.com>
2021-10-05 14:27:27 -0700

    Add files via upload

--- screens/MovieDetailScreen/MovieDetailScreen.main.jsx
+++ screens/MovieDetailScreen/MovieDetailScreen.main.jsx
@@ -4,12 +4,21 @@ import { styles } from "./MovieDetailScreen.styles";
 
 export default function MovieDetailScreen({ route }) {
   // TODO: Recieve the movieItem by destructuring route params.
+  /* TODO: Configure this screen to have an image and appropriate text describing the movie. 
+      See the example on the spec for design inspiration.
+      Feel free to use the styles below. */
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.container}>
-        {/* TODO: Configure this screen to have an image and appropriate text describing the movie. 
-                See the example on the spec for design inspiration.
-                Feel free to use the styles below. */}
+        <Image
+          style={{ width: 216, height: 320 }}
+          source={{ uri: route.params.posterurl }}
+        />
+        <Text styles={styles.h1}>{route.params.title}</Text>
+        <Text styles={styles.h2}>Released {route.params.year}</Text>
+        <Text styles={styles.h2}>{route.params.genres}</Text>
+        <Text styles={styles.h2}>{route.params.actors}</Text>
+        <Text styles={styles.h4}>{route.params.storyline}</Text>
       </ScrollView>
     </SafeAreaView>
   );
